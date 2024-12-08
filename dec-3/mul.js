9c9561332afe0e3a1126b918f7945a5660a78f25
Jesper Broni Andersen <jba@stibodx.com>
2024-12-08 10:50:48 +0100

    Solve for dec-3.2
    Add checks for do() and don't() instructions and filter out muls that
    are disabled.

--- dec-3/mul.js
+++ dec-3/mul.js
@@ -3,12 +3,22 @@ const fs = require('fs');
 try {
     const input = fs.readFileSync('input.txt', 'utf8');
 
-    const result = [...input.matchAll(/mul\([0-9]+,[0-9]+\)/g)]
-        .map(value => value[0]) // Get all mul values
-        .map(value => value.slice(4, -1)) // Remove mul( and )
-        .map(value => value.split(',').map(Number)) // Split and map to numbers
-        .reduce((previousValue, currentValue) => previousValue + (currentValue[0] * currentValue[1]), 0);
+    const instructions = [...input.matchAll(/mul\([0-9]+,[0-9]+\)|do\(\)|don't\(\)/g)]
+        .map(value => value[0]); // Get all instructions
 
+    let enabled = true;
+    let muls = [];
+    instructions.forEach(value => {
+        if (value === "don't()") {
+            enabled = false;
+        } else if (value === 'do()') {
+            enabled = true;
+        } else if (enabled) { // mul instruction
+            muls.push(value.slice(4, -1).split(',').map(Number));
+        }
+    });
+
+    const result = muls.reduce((previousValue, currentValue) => previousValue + (currentValue[0] * currentValue[1]), 0);
     console.log('Result: ', result);
 } catch (error) {
     console.error('Error reading input: ', error);
